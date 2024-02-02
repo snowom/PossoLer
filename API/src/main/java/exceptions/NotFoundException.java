75c1152cd192f9be2cb53e9f09a4f45977255557
thomazf <thomaz.fneves@gmail.com>
2024-02-01 22:17:45 -0300

    separação repositorio API

--- API/src/main/java/exceptions/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class NotFoundException extends RuntimeException{
-
-    public NotFoundException(String message)
-    {
-        super(message);
-    }
-}
--- API/src/main/java/exceptions/ServerErrorException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class ServerErrorException extends RuntimeException{
-
-    public ServerErrorException(String message)
-    {
-        super(message);
-    }
-}
