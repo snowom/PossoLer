eb950a2a7c9ee76ada8fcd7726e59eaf63648230
thomazf <thomaz.fneves@gmail.com>
2024-02-17 01:17:20 -0300

    migração serviço de contagem de acesso de sites desbloqueados

--- web/possoLer.user.js
+++ web/possoLer.user.js
@@ -80,6 +80,7 @@
 // @require      http://localhost:8080/API/getCDN?file=att_versao
 // @require      http://localhost:8080/API/getCDN?file=check_messages
 // @require      http://localhost:8080/API/getCDN?file=count_content
+// @require      http://localhost:8080/API/getCDN?file=save_site_access
 // @grant        GM_webRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -363,87 +364,6 @@ function importCDNSnackBar()
 }
 
 
-/* ========================== API INCREMENTO DE INFOS DASHBOARD SITE ====================== */
-
-function getCurrentDate()
-{
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yyyy = today.getFullYear();
-    today = `${yyyy}-${mm}-${dd}`;
-
-    return today;
-}
-
-
-function getCurrentTime()
-{
-    let today = new Date();
-    let hh = String(today.getHours()).padStart(2, '0');
-    let mm = String(today.getMinutes()).padStart(2, '0');
-    let ss = String(today.getSeconds()).padStart(2, '0');
-    today = `${hh}:${mm}:${ss}`;
-
-    return today;
-}
-
-
-function saveDataForDashboard(codigoSite)
-{
-    let currentData = getCurrentDate();
-    let currentTime = getCurrentTime();
-    const ENDPOINT_DASHBOARD = 'https://possoler.tech/API/acessos/insertDadosAccess';
-
-    let r = setInterval(()=>{
-        if(currentData != null && currentTime != null){
-            clearInterval(r);
-            if(typeof(axios) == 'function'){
-                axios({
-                    method: 'POST',
-                    url: ENDPOINT_DASHBOARD,
-                    timeout: 30000,
-                    data: JSON.stringify({
-                        codigo_site: codigoSite,
-                        data: currentData,
-                        horario: currentTime
-                    }),
-                    headers : {
-                        "Content-Type" : "application/json"
-                    }
-                }).then((resp)=>{
-                    if(resp.status == 201){
-                        console.log('[AXIOS] DASHBOARD DATA OK!');
-                    }else{
-                        console.log('[AXIOS] DASHBOARD DATA ERROR!');
-                    }
-                }).catch((erro)=>{
-                    console.log('[AXIOS] FALHA DASHBOARD!', erro.toString());
-                });
-            }else if(self.fetch){
-                fetch(ENDPOINT_DASHBOARD,
-                {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        codigo_site: codigoSite,
-                        data: currentData,
-                        horario: currentTime
-                    })
-                }).then(function(res){
-                    if(res.status == 201) {
-                        console.log('[FETCH] DASHBOARD DATA OK!');
-                    }else{
-                        console.log('[FETCH] DASHBOARD DATA ERROR!');
-                    }
-                }).catch((erro)=>{
-                    console.log('[FETCH] FALHA DASHBOARD!', erro.toString());
-                });
-            }else{
-                console.log('SEM AXIOS E FETCH DASHBOARD');
-            }
-        }
-    },800);
-}
 
 
 
