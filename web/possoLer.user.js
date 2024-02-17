f9df30fc177ba7caecb8de1a005595a785921d22
thomazf <thomaz.fneves@gmail.com>
2024-02-16 21:59:25 -0300

    migração JOTA

--- web/possoLer.user.js
+++ web/possoLer.user.js
@@ -66,6 +66,7 @@
 // @require      http://localhost:8080/API/getCDN?file=respondeai
 // @require      http://localhost:8080/API/getCDN?file=valorecon
 // @require      http://localhost:8080/API/getCDN?file=exame
+// @require      http://localhost:8080/API/getCDN?file=jota
 // @grant        GM_webRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -1300,105 +1301,7 @@ function modifyNYTIMES()
 
 /* ======================= JOTA ================================= */
 
-function modifyJOTA()
-{
-    let rotina = setInterval(()=>{
-        if(verificaElemento("head")) {
-            clearInterval(rotina);
 
-            let links = document.head.querySelectorAll("link");
-            let linkJSON;
-
-            for(let i=0; i<links.length; i++)
-            {
-                if(links[i].getAttribute("href").includes("https://www.jota.info/wp-json/wp/v2/posts/"))
-                {
-                    linkJSON = links[i].getAttribute("href");
-                    getJsonConteudoNoticia(linkJSON);
-                    break;
-                }
-            }
-            removeBloqueioPaywallJOTA();
-            verificaAtualizacaoVersao();
-        }
-    },800);
-}
-
-
-function getJsonConteudoNoticia(link)
-{
-    let rotina = setInterval(()=>{
-        if(typeof(axios) == 'function') {
-            clearInterval(rotina);
-
-            axios({
-                method: "GET",
-                url: link,
-                timeout: 15000
-            }).then((resp)=>{
-                let respFunction = setNoticiaContainerJOTA(resp.data.content.rendered);
-
-                let rotina = setInterval(()=>{
-                    if (respFunction != null || respFunction != undefined) {
-                        clearInterval(rotina);
-                        console.log("TENHO RESPOSTA FUNCTION JOTA = "+respFunction);
-                        removeBannerProJOTA();
-                    }
-                },800);
-
-            }).catch((erro)=>{
-                console.error(erro);
-            });
-        }
-    },800)
-}
-
-
-function setNoticiaContainerJOTA(noticia)
-{
-    let childrenContainer = document.querySelector(".jota-article__content").children;
-    let arrayPossibleAttributes = ["style", "data-amp-original-style"];
-
-    for(let i=0; i<childrenContainer.length; i++) {
-        for(let j=0; j<arrayPossibleAttributes.length; j++) {
-            if(childrenContainer[i].hasAttribute(arrayPossibleAttributes[j])) {
-                if(childrenContainer[i].getAttribute(arrayPossibleAttributes[j]).includes("font-weight:400"))
-                {
-                    childrenContainer[i].innerHTML = `${noticia}`;
-                    incrementaConteudoAPI();
-                    return 1;
-                }
-            }
-        }
-    }
-
-    return 0;
-}
-
-
-function removeBloqueioPaywallJOTA()
-{
-    let rotina = setInterval(()=>{
-        if(verificaElemento(".jota-paywall__wrap"))
-        {
-            clearInterval(rotina);
-            document.querySelector(".jota-paywall__wrap").remove();
-        }
-    },800);
-}
-
-
-function removeBannerProJOTA()
-{
-    let links = document.querySelectorAll("a");
-
-    for(let i=0; i<links.length; i++) {
-        if(links[i].getAttribute("href").includes("source=Banner"))
-        {
-            links[i].parentElement.remove();
-        }
-    }
-}
 
 
 
