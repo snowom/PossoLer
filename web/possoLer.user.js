f427ca5a0462c3e94b87d8b5c164b21dc3b208f6
thomazf <thomaz.fneves@gmail.com>
2024-02-17 00:41:14 -0300

    migração api atualizacao versao

--- web/possoLer.user.js
+++ web/possoLer.user.js
@@ -77,6 +77,7 @@
 // @require      http://localhost:8080/API/getCDN?file=opovo
 // @require      http://localhost:8080/API/getCDN?file=gaz
 // @require      http://localhost:8080/API/getCDN?file=possoler
+// @require      http://localhost:8080/API/getCDN?file=att_versao
 // @grant        GM_webRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -89,7 +90,7 @@
 importCDNSnackBar();
 let currentURL = window.location.hostname;
 const DOMAIN = "http://localhost:8080";
-const CURRENT_VERSION = '297';
+const CURRENT_VERSION = '298';
 
 
 function main()
@@ -359,59 +360,6 @@ function importCDNSnackBar()
     document.head.appendChild(sweetAlertJS);
 }
 
-
-function configSnackBar(msg, tituloBtn, tempo)
-{
-    return {
-            text: msg,
-            actionTextColor: '#a1ff00',
-            actionText: tituloBtn,
-            pos: 'top-right',
-            duration: tempo*1000,
-            customClass: 'snackBarMsg',
-            onActionClick: ()=>{
-                window.open('https://possoler.tech/#blockDownload');
-            }
-        };
-}
-
-function verificaAtualizacaoVersao()
-{
-    if(window.location.href.includes('possoler.tech')) return;
-
-    let rotina = setInterval(()=>{
-        if(typeof(Snackbar) == 'object'  && verificaElemento('#snackCSS') && typeof(axios) == 'function' && typeof(Swal) == 'function') {
-            clearInterval(rotina);
-
-            const URL_API_UPDATE = 'https://possoler.tech/API/searchUpdates';
-            let tempoAwait = 5;
-
-            axios({
-                method: 'GET',
-                url: URL_API_UPDATE,
-                timeout: 20000,
-            }).then((resposta)=>{
-                let updateVersion = resposta.data.currentVersion;
-                let msgUpdate = resposta.data.message;
-                let tituloBtn = resposta.data.buttonMessage;
-                let tempo = resposta.data.time;
-
-                if(CURRENT_VERSION<updateVersion){
-
-                    tempoAwait = 12;
-                    let options = configSnackBar(msgUpdate,tituloBtn,tempo);
-                    Snackbar.show(options);
-                }
-
-                verificaMensagensAPI(tempoAwait);
-
-            }).catch((erro)=>{
-                console.error(erro);
-                verificaMensagensAPI(tempoAwait);
-            });
-        }
-    }, 800);
-}
 
 
 function verificaMensagensAPI(time)
