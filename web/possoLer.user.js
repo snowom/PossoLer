603511063a9077a9c15970ec33d4d88c14929099
thomazf <thomaz.fneves@gmail.com>
2024-02-16 21:49:21 -0300

    migração revista exame

--- web/possoLer.user.js
+++ web/possoLer.user.js
@@ -65,6 +65,7 @@
 // @require      http://localhost:8080/API/getCDN?file=app_respondeai
 // @require      http://localhost:8080/API/getCDN?file=respondeai
 // @require      http://localhost:8080/API/getCDN?file=valorecon
+// @require      http://localhost:8080/API/getCDN?file=exame
 // @grant        GM_webRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -1419,39 +1420,7 @@ function modifyPossoLer()
 
 /* ======================= EXAME ================================ */
 
-function modifyEXAME()
-{
-    verificaAtualizacaoVersao();
 
-    let r = setInterval(()=>{
-        if(document.querySelector(".xm-paywall") != null && typeof(__NEXT_DATA__) == "object"){
-            clearInterval(r);
-            try{
-                let unlockedContent = "";
-                (__NEXT_DATA__.props.pageProps.postOrSubcategoryProps.postData).forEach((data)=>{
-                    unlockedContent += (data.hasOwnProperty("content")) ? data.content : "";
-                });
-                document.querySelector(".news-content-container").innerHTML = unlockedContent;
-                incrementaConteudoAPI();
-            }catch(erro){
-                let s = setInterval(()=>{
-                    if(typeof(Swal) == 'function'){
-                        clearInterval(s);
-                        console.log('ACHEI SWALL');
-
-                        if(Swal.isVisible() == false){
-                            sweetAlert(
-                                'error',
-                                'Erro',
-                                `Ops, tivemos um pequeno problema!<br>Por favor, tente novamente mais tarde.<br><br><spam style='font-weight: bold !important;'>Código do erro: </spam>${erro.toString()}`
-                            );
-                        }
-                    }
-                },800);
-            }
-        }
-    },800)
-}
 
 
 
