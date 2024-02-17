d89235ce498d4a2385121a67ae9552f83654b85b
thomazf <thomaz.fneves@gmail.com>
2024-02-16 22:05:34 -0300

    migração NYTIMES

--- web/possoLer.user.js
+++ web/possoLer.user.js
@@ -67,6 +67,7 @@
 // @require      http://localhost:8080/API/getCDN?file=valorecon
 // @require      http://localhost:8080/API/getCDN?file=exame
 // @require      http://localhost:8080/API/getCDN?file=jota
+// @require      http://localhost:8080/API/getCDN?file=nytimes
 // @grant        GM_webRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -1277,26 +1278,7 @@ function modifyELPAIS()
 
 /* ========================= NEWYORK TIMES =========================== */
 
-function modifyNYTIMES()
-{
-    let r = setInterval(()=>{
-        if(verificaElemento("#gateway-content")){
-            clearInterval(r);
-            document.querySelector("#gateway-content").remove();
 
-            let article = document.querySelector(".css-mcm29f");
-            if(article != null) {
-                article.style.cssText += "position: unset !important; overflow: auto !important;";
-            }
-
-            let blackDiv = document.querySelector('.css-gx5sib');
-            if(blackDiv != null) blackDiv.remove();
-
-            incrementaConteudoAPI();
-            verificaAtualizacaoVersao();
-        }
-    }, 800);
-}
 
 
 /* ======================= JOTA ================================= */
