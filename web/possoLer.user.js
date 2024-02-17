40a33871f42d299715535d6abebc0751173bf38e
thomazf <thomaz.fneves@gmail.com>
2024-02-16 23:47:17 -0300

    migração diario santa maria

--- web/possoLer.user.js
+++ web/possoLer.user.js
@@ -71,6 +71,7 @@
 // @require      http://localhost:8080/API/getCDN?file=elpais
 // @require      http://localhost:8080/API/getCDN?file=gauchazh
 // @require      http://localhost:8080/API/getCDN?file=opopular
+// @require      http://localhost:8080/API/getCDN?file=diariosm
 // @grant        GM_webRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -799,77 +800,7 @@ function restoreImgs()
 
 /* ============================== DIARIO SANTA MARIA ============================== */
 
-function modifyDIARIOSM()
-{
-    //ADD JQUERY JS
-    let jqueryJS = document.createElement('script');
-    jqueryJS.setAttribute('src', 'https://code.jquery.com/jquery-3.6.0.min.js');
-    document.body.appendChild(jqueryJS);
 
-    let r = setInterval(()=>{
-        if(verificaElemento('.row-bloqueio') && typeof($) == 'function' && findPaywallText()){
-            clearInterval(r);
-
-            try{
-                let base64ParsedBody = CryptoJS.enc.Base64.parse(parsedBody);
-                let cryptoKey = CryptoJS.enc.Base64.parse('u/Gu5posvwDsXUnV5Zaq4g==');
-                let cryptoIv = CryptoJS.enc.Base64.parse('5D9r9ZVzEYYgha93/aUK2w==');
-
-                $(".suita-artigo .row-conteudo .post-content").html(CryptoJS.enc.Utf8.stringify(CryptoJS.AES.decrypt({
-                    ciphertext: base64ParsedBody
-                }, cryptoKey, {
-                    mode: CryptoJS.mode.CBC,
-                    padding: CryptoJS.pad.Pkcs7,
-                    iv: cryptoIv,
-                })));
-
-                $(".suita-artigo .row-conteudo").show();
-                $(".suita-artigo .videos").show();
-                $(".suita-artigo .imagens").show();
-                $(".suita-artigo .elementos").show();
-                $(".suita-artigo .row-loading").hide();
-
-                let u = setInterval(()=>{
-                    if(typeof(twttr) == 'object'){
-                        clearInterval(u);
-                        twttr.widgets.load();
-                    }
-                },800);
-
-                document.querySelector('.row-bloqueio').remove();
-                incrementaConteudoAPI();
-                verificaAtualizacaoVersao();
-
-            }catch(erro){
-                if(typeof(Swal) == 'function'){
-                    sweetAlert(
-                        'error',
-                        'Erro',
-                        `Ops, tivemos um pequeno problema!<br>Por favor, tente novamente mais tarde.<br><br><spam style='font-weight: bold !important;'>Código do erro: </spam>${erro.toString()}`
-                    );
-                }else{
-                    alert(erro.toString());
-                }
-            }
-        }
-    },800);
-}
-
-
-function findPaywallText()
-{
-    let h1 = document.querySelectorAll('h1');
-    for(let i=0; i<h1.length; i++){
-        if(h1[i].textContent.includes("Conteúdo exclusivo!")){
-            return true;
-        }
-    }
-    return false;
-}
-
-
-
-/* ============================== O POPULAR ============================== */
 
 
 
